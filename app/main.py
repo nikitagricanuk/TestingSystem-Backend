60452f99b224183dde0b07f3bdadcc40096c6563
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-09-04 16:17:34 +0800

    Added some test code

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from app.db.dao.userdao import UserDAO, RoleEnum
+
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    user = await UserDAO().create(first_name="John", middle_name="B.", second_name="Doe",
+                                  age=20, email="my@ngctl.ru", phone="1234562890",
+                                  password="securepassword123", role=RoleEnum.ADMIN, school_id=None
+                                  )
+    return {"Hello": user.first_name}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
