6ca51a899e9dc3746f8d59bb1ff912f3e4c080a3
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-10-09 14:35:17 +0800

    Added DATABASE_URL constant

--- app/core/databases.py
+++ app/core/databases.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from app.core.config import settings
 
 
-DATABASE_URL = settings.get_db_url()
+DATABASE_URL = settings.get_postgres_url
 
 engine = create_async_engine(url=DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
