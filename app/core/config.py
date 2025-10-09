88441f6c6c322360c47336302a05dc4128ec351a
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-10-09 14:34:49 +0800

    Added get_postgres_url() func

--- app/core/config.py
+++ app/core/config.py
@@ -24,4 +24,9 @@ class Settings(BaseSettings):
             f"@{self.redis_host}:{self.redis_port}"
         )
 
+    @property
+    def get_postgres_url(self):
+        return (f"postgresql+asyncpg://{self.db_user}:{self.db_password}@"
+                f"{self.db_host}:{self.db_port}/{self.db_name}")
+
 settings = Settings()
