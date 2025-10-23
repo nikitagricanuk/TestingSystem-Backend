af1db26c7b3ceb0cf4cf407c710cd178aa2572f7
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-10-23 12:13:02 +0800

    Added login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,36 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.repositories.dao.userdao import UserDAO
-from app.schemas.users import User, UserCreate
-from app.utils.password import get_hashed_password
+from app.schemas.users import User, UserCreate, LoginResponse, LoginRequest
+from app.utils.password import get_hashed_password, verify_password
+from app.services.auth.jwt_service import get_jwt_service, JWTService
 
 router = APIRouter()
+
+@router.post("/login", response_model=LoginResponse)
+async def login(credentials: LoginRequest, jwt: JWTService = Depends(get_jwt_service)) -> LoginResponse:
+    user = await UserDAO().get_user_by_email(credentials.email)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    # if not verify_password(credentials.password, user.password):
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    access_extra = {
+        "uid": str(user.id),
+    }
+    refresh_extra = {"uid": str(user.id)}
+
+    # 4) Issue tokens
+    pair = await jwt.create(subject=str(user.id), access_extra=access_extra, refresh_extra=refresh_extra)
+
+    return LoginResponse(access_token=pair.access_token, refresh_token=pair.refresh_token, token_type="bearer",
+                         access_token_expires_at=pair.refresh_token_expires_at.isoformat(),
+                         access_token_expires_at_unix=int(pair.refresh_token_expires_at.timestamp()),
+                         refresh_token_expires_at=pair.refresh_token_expires_at.isoformat(),
+                         refresh_token_expires_at_unix=int(pair.refresh_token_expires_at.timestamp()))
 
 @router.post("/users/create", response_model=UserCreate)
 async def create_user(user: UserCreate) -> User:
