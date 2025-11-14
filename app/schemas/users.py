4c5f90d2441931c1ed461622c393eec942c4abf2
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2025-11-14 16:05:09 +0800

    Added new models

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -8,19 +8,32 @@ from app.utils.time import get_current_time
 
 class User(BaseModel):
     id: UUID
+    nickname: str
     email: str
     is_active: bool
     role: str | None = None
-    permissions: list[str] = []
     created_at: datetime
     created_at_unix: int
     updated_at: datetime | None = None
     updated_at_unix: int | None = None
 
+class UserShort(BaseModel):
+    id: UUID
+    full_name: str
+    email: str
+    is_active: bool
+    role: str | None = None
+
+class UserFull(User):
+    full_name: str
+    age: int | None = None
+    phone: str | None = None
+    school_id: UUID | None = None
+    permissions: list[str] = []
+
 class UserCreate(BaseModel):
-    first_name: str
-    middle_name: str | None = None
-    second_name: str
+    full_name: str
+    nickname: str
     age: int | None = None
     phone: str | None = None
     school_id: str | None = None
@@ -36,9 +49,22 @@ class UserCreateStudent(BaseModel):
     second_name: str
     age: int | None = None
     phone: str | None = None
+    school_id: UUID | None = None
+    email: str
+    password: str
+
+class UserUpdate(BaseModel):
+    first_name: str
+    middle_name: str | None = None
+    last_name: str
+    age: int | None = None
+    phone: str | None = None
     school_id: str | None = None
     email: str
     password: str
+    is_active: bool = True
+    role: UUID | None = None
+    additional_permissions: list[str] = []
 
 class LoginRequest(BaseModel):
     email: str
