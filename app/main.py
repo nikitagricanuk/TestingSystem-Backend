0a2c3de43c1aa99fdea1d1f9104a41e1fede07a4
Nikita Gritsanyuk <doting.laws0d@icloud.com>
2026-02-01 12:48:14 +0800

    Fixed include paths

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from .routers import results, auth
+from .routers import results
 from .services.testing_engine.routers import sessions
+from .services.auth.routers import auth
 
 
 def create_app() -> FastAPI:
